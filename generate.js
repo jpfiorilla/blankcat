65b25af61ec79bb63fd6d7a073abc3306066bcf7
John Fiorilla <john.fiorilla@loan-street.com>
2018-03-20 11:43:27 -0400

    gens nouns & verbs init

--- generate.js
+++ generate.js
@@ -1,6 +1,52 @@
 const rita = require('rita');
+const fs = require('fs');
+require.extensions['.txt'] = function(module, filename) {
+  module.exports = fs.readFileSync(filename, 'utf8');
+};
+const dictionary = require('./dictionary.txt').split('\n');
 
-const generate = () => 'hi';
+const randomIndex = (max, excepts) => Math.floor(Math.random() * (max + 1));
+Array.prototype.pick = () => this[Math.floor(Math.random() * this.length)];
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+}
+
+const madlib = (noun, verb) => `${noun} cat. The cat that ${verb} ${noun}!`;
+
+const nouns = dictionary
+  .filter(word => !word.includes(' ') && rita.isNoun(word))
+  .map(word => rita.singularize(word))
+  .filter(onlyUnique);
+
+const verbs = dictionary
+  .filter(word => !word.includes(' ') && rita.isVerb(word))
+  .map(word =>
+    rita.conjugate(word, {
+      tense: rita.PRESENT_TENSE,
+      number: rita.SINGULAR,
+      person: rita.THIRD_PERSON
+    })
+  )
+  .filter(onlyUnique);
+
+const generate = () => verbs;
+
+// const plurals = nouns.map(singular => rita.pluralize(singular));
+
+// const singulars = plurals.map(plural => rita.singularize(plural));
+
+// const generate = () =>
+//   console.log(
+//     singulars.map((singular, i) => `${nouns[i]} ${plurals[i]} ${singular}`)
+//   );
+
+// const generate = () =>
+//   fs.writeFile('/nouns.txt', nounGen.join('\n'), function(err) {
+//     if (err) {
+//       return console.log(err);
+//     }
+//     console.log('The file was saved!');
+//   });
 
 module.exports = {
   generate
--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const T = new Twit({
   timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
 });
 
-const madlib = (noun, verb) => `${noun} cat. The cat that ${verb} ${noun}!`;
-
 const post = status =>
   T.post('statuses/update', { status }, (err, data, response) => {
     console.log(data);
