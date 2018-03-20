5c6001e439b62643a7471e8ef953f556723bf091
John Fiorilla <john.fiorilla@loan-street.com>
2018-03-20 12:57:46 -0400

    nouns & verbs txt filtered

--- generate.js
+++ generate.js
@@ -5,48 +5,59 @@ require.extensions['.txt'] = function(module, filename) {
 };
 const dictionary = require('./dictionary.txt').split('\n');
 
+const capitalize = word => word[0].toUpperCase() + word.substr(1);
 const randomIndex = (max, excepts) => Math.floor(Math.random() * (max + 1));
-Array.prototype.pick = () => this[Math.floor(Math.random() * this.length)];
-function onlyUnique(value, index, self) {
+Array.prototype.pick = function() {
+  return this[Math.floor(Math.random() * this.length)];
+};
+function isUnique(value, index, self) {
   return self.indexOf(value) === index;
 }
 
-const madlib = (noun, verb) => `${noun} cat. The cat that ${verb} ${noun}!`;
-
 const nouns = dictionary
-  .filter(word => !word.includes(' ') && rita.isNoun(word))
-  .map(word => rita.singularize(word))
-  .filter(onlyUnique);
-
-const verbs = dictionary
-  .filter(word => !word.includes(' ') && rita.isVerb(word))
-  .map(word =>
-    rita.conjugate(word, {
-      tense: rita.PRESENT_TENSE,
-      number: rita.SINGULAR,
-      person: rita.THIRD_PERSON
-    })
+  .filter(
+    word =>
+      !word.includes(' ') &&
+      rita.isNoun(word) &&
+      rita.getPosTags(word).includes('nn')
   )
-  .filter(onlyUnique);
+  .map(word => rita.singularize(word))
+  .filter(isUnique);
 
-const generate = () => verbs;
+// const verbs = dictionary
+//   .filter(word => !word.includes(' ') && rita.getPosTags(word).includes('vb'))
+//   .map(word =>
+//     rita.conjugate(word, {
+//       tense: rita.PRESENT_TENSE,
+//       number: rita.SINGULAR,
+//       person: rita.THIRD_PERSON
+//     })
+//   )
+//   .filter(isUnique);
 
-// const plurals = nouns.map(singular => rita.pluralize(singular));
+// const noun = nouns.pick();
+// const verb = verbs.pick();
 
-// const singulars = plurals.map(plural => rita.singularize(plural));
+const madlib = (noun, verb) =>
+  `${capitalize(noun)} cat. The cat that ${verb} ${rita.pluralize(noun)}!`;
 
-// const generate = () =>
+// const generate = () => {
 //   console.log(
-//     singulars.map((singular, i) => `${nouns[i]} ${plurals[i]} ${singular}`)
+//     nouns
+//       .map((noun, i) => `${noun} ${verbs[i]}`)
+//       .slice(0, 100)
+//       .join('\n')
 //   );
+//   return madlib(noun, verb);
+// };
 
-// const generate = () =>
-//   fs.writeFile('/nouns.txt', nounGen.join('\n'), function(err) {
-//     if (err) {
-//       return console.log(err);
-//     }
-//     console.log('The file was saved!');
-//   });
+const generate = () =>
+  fs.writeFile('./nouns.txt', nouns.join('\n'), function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('The file was saved!');
+  });
 
 module.exports = {
   generate
