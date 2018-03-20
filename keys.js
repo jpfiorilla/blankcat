c155d6447aa3140cddc48b835fc3e55bbd1cf9ec
John Fiorilla <john.fiorilla@loan-street.com>
2018-03-20 13:26:16 -0400

    heroku

---
+++ keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key: 'oOBOjsIiQY7ZS79SpzVbJfhGG',
+  consumer_secret: 'zem7ilakvQThGUXiadL2DxJCwSpubfLZksgsNRVLkcyQ6iiBZo',
+  access_token: '976141761620992000-2WUzgJzxmgHrX2ItYmQRWebPtMUkybG',
+  access_token_secret: 'm8s0a0ihGtDq6uTzgNCN5Q2rDOYJo5HhKOTZrsfDy4FD4',
+  timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
+};
